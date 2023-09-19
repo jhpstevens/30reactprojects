6d8d8783c6ddf6546536abb6c6a32ec2a5c7dfac
jhpstevens <49429079+jhpstevens@users.noreply.github.com>
2023-09-19 15:14:42 +0200

    E-signature-app-with-useState

--- src/projects/01-e-signature-app/EsignatureApp.jsx
+++ src/projects/01-e-signature-app/EsignatureApp.jsx
@@ -1,18 +1,28 @@
-import React from "react";
-import Title from "../components/Title";
+import React, { useState } from 'react';
+import Title from '../components/Title';
 
 export default function EsignatureApp() {
-  const inputStyle = {
-    border: "none",
-    borderBottom: "2px dotted",
-    outline: "none",
-    padding: ".35rem 0",
+  const [name, setName] = useState('Your Signature');
+  const [date, setDate] = useState('Select date');
+  const handleNameChange = (e) => {
+    // console.log(e.target.value);
+    setName(e.target.value);
   };
-  document.body.style.background = "#eee";
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+  const inputStyle = {
+    border: 'none',
+    borderBottom: '2px dotted',
+    outline: 'none',
+    padding: '.35rem 0',
+  };
+  document.body.style.background = '#eee';
+
   return (
     <div className="container text-center">
-      <Title classes={"title"} text={"Name"} />
-      <Title classes={"main-title mb-4"} text={"Date"} />
+      <Title classes={'title'} text={name} />
+      <Title classes={'main-title mb-4'} text={!date ? 'DoB' : date} />
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore aut
         debitis voluptatem, minima voluptate eligendi optio commodi totam
@@ -22,13 +32,23 @@ export default function EsignatureApp() {
       <footer
         className="d-flex"
         style={{
-          justifyContent: "space-around",
-          position: "relative",
-          top: "40vh",
+          justifyContent: 'space-around',
+          position: 'relative',
+          top: '40vh',
         }}
       >
-        <input type="date" value={""} style={inputStyle} />
-        <input type="text" value={""} style={inputStyle} />
+        <input
+          type="date"
+          value={date}
+          style={inputStyle}
+          onChange={handleDateChange}
+        />
+        <input
+          type="text"
+          value={name}
+          style={inputStyle}
+          onChange={handleNameChange}
+        />
       </footer>
     </div>
   );
