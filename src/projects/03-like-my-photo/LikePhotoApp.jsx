82702c5699ecfc0f3d1e342f69dc87b907e03666
jhpstevens <49429079+jhpstevens@users.noreply.github.com>
2023-09-20 19:22:17 +0200

    1 binaries uploaded

--- src/projects/03-like-my-photo/LikePhotoApp.jsx
+++ src/projects/03-like-my-photo/LikePhotoApp.jsx
@@ -1,35 +1,37 @@
-import React from "react";
-import Title from "../components/Title";
+import React from 'react';
+import Title from '../components/Title';
+import DogyDog from './img/dog.jpg';
+
 import {
   AiFillSmile,
   AiFillHeart,
   AiOutlineHeart,
   AiOutlineComment,
-} from "react-icons/ai";
+} from 'react-icons/ai';
 
-import DogyDog from "./img/dog.jpg";
 
-let  like=false;
+
+let like = false;
 
 export default function LikePhotoApp() {
   return (
     <div className="container text-center">
-      <Title text={"LikePhotoApp"} />
-      <Title classes={"subtitle"} text={"Likes"} />
+      <Title text={'LikePhotoApp'} />
+      <Title classes={'subtitle'} text={'Likes'} />
       <div
         className="card card-dark m-auto"
-        style={{ width: 300, cursor: "pointer" }}
+        style={{ width: 300, cursor: 'pointer' }}
       >
         <div className="card-header fs-xl">
-          {" "}
+          {' '}
           <AiFillSmile className="mr-2" />
           <small>DogyDog</small>
         </div>
 
-        <img src={DogyDog} alt="img" style={{ height: "fit-content" }} />
+        <img src={DogyDog} alt="img" style={{ height: 'fit-content' }} />
         <div
           className="card-footer fs-xl d-flex"
-          style={{ justifyContent: "space-between" }}
+          style={{ justifyContent: 'space-between' }}
         >
           <AiOutlineComment />
           {like ? <AiFillHeart className="text-danger" /> : <AiOutlineHeart />}
